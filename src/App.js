5f812b72d430062489d38be3aa515dfe6985a421
Kaiky <info.kaiky@gmail.com>
2021-01-21 14:40:22 -0300

    Layout base finalizado

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+import { commerce } from './lib/commerce';
+
+//import NavBar from './components/Navbar/Navbar';
+//import Products from './components/Products/Products';
+import { Products, Navbar } from './components';
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <Products products={products} />
+    </div>
+  )
+}
+
+export default App;
